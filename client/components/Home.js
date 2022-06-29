7203ed1496607762d237d83e6382bf5137d2cae1
Erion-X <98784085+Erion-X@users.noreply.github.com>
2022-06-29 16:24:27 -0400

    rendering google maps on HomeComponent successfully.

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,26 +1,53 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-/**
- * COMPONENT
- */
-export const Home = props => {
-  const {username} = props
+const MapContainer = () => {
+  const mapStyles = {
+    height: '100vh',
+    width: '100%',
+  };
+
+  const defaultCenter = {
+    lat: 40.73061,
+    lng: -73.935242,
+  };
 
   return (
-    <div>
-      <h3>Welcome, {username}</h3>
-    </div>
-  )
-}
+    <LoadScript googleMapsApiKey="AIzaSyDd5tCW_eNmH7IGrTQvgsSs4v5QS1VTank">
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        zoom={15}
+        center={defaultCenter}
+      />
+    </LoadScript>
+  );
+};
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    username: state.auth.username
-  }
-}
+export default MapContainer;
 
-export default connect(mapState)(Home)
+// import React from 'react'
+// import {connect} from 'react-redux'
+
+// /**
+//  * COMPONENT
+//  */
+// export const Home = props => {
+//   const {username} = props
+
+//   return (
+//     <div>
+//       <h3>Welcome, {username}</h3>
+//     </div>
+//   )
+// }
+
+// /**
+//  * CONTAINER
+//  */
+// const mapState = state => {
+//   return {
+//     username: state.auth.username
+//   }
+// }
+
+// export default connect(mapState)(Home)
