dc0880df1e0c6dc9c8a3bd9af0c911eef6b02f20
walkerfisher <101424637+walkerfisher@users.noreply.github.com>
2022-06-29 15:06:33 -0700

    Seed, Models and Associations Done

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,19 @@
-//this is the access point for all things database related!
+const db = require("./db");
 
-const db = require('./db')
+const User = require("./models/User");
 
-const User = require('./models/User')
+const Event = require("./models/Event");
 
-//associations could go here!
+const UsersEvents = require("./models/UsersEvents");
+
+User.belongsToMany(Event, { through: UsersEvents });
+Event.belongsToMany(User, { through: UsersEvents });
 
 module.exports = {
   db,
   models: {
     User,
+    Event,
+    UsersEvents,
   },
-}
+};
