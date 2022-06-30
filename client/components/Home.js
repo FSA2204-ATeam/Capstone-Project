2ab2a88cf63af4cb982b2ddd79cea6d79fd3fc46
Alessandro De Mitri <90a.demitri@gmail.com>
2022-06-30 11:59:49 -0500

    markers on maps working

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,61 +1,4 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const MapContainer = () => {
-  const styles = [
-    {
-      featureType: 'poi.business',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'road',
-      elementType: 'labels.icon',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-    {
-      featureType: 'transit',
-      stylers: [
-        {
-          visibility: 'off',
-        },
-      ],
-    },
-  ];
-
-  //Get client location - (need to incorporate ask permission)
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  });
-  const mapStyles = {
-    height: '100vh',
-    width: '100%',
-  };
-
-  const defaultCenter = {
-    lat: 40.73061,
-    lng: -73.935242,
-  };
-
-  return (
-    <LoadScript googleMapsApiKey="AIzaSyDd5tCW_eNmH7IGrTQvgsSs4v5QS1VTank">
-      <GoogleMap
-        mapContainerStyle={mapStyles}
-        zoom={15}
-        center={defaultCenter}
-      />
-    </LoadScript>
-  );
-};
-
-export default MapContainer;
 
 // import React from 'react'
 // import {connect} from 'react-redux'
@@ -83,3 +26,114 @@ export default MapContainer;
 // }
 
 // export default connect(mapState)(Home)
+
+import React, { Component } from 'react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+let  events = [
+{
+"title" : "Discoteque",
+"lat": "40.7589",
+"lng":"-73.9851",
+},
+
+{
+"title" : "Jam Session",
+"lat": "40.7690",
+"lng":"-73.9952",
+},
+{
+"title" : "Art Craft session",
+"lat": "40.7489",
+"lng":"-73.9751",
+}
+]
+
+class MapContainer extends Component {
+state = {
+showingInfoWindow: false,
+activeMarker: {},
+selectedPlace: {},
+query:'',
+filteredPlaces: []
+};
+
+markers = []
+
+onMarkerClick = (props, marker, e) => {
+this.setState({
+  selectedPlace: props,
+  activeMarker: marker,
+  showingInfoWindow: true
+ });
+}
+
+onLiClick = (i) =>{
+this.setState({
+    showingInfoWindow: true,
+    activeMarker: this.markers[i],
+    selectedPlace: events[i]
+})
+}
+
+onMapClicked = (props) => {
+if (this.state.showingInfoWindow) {
+  this.setState({
+    showingInfoWindow: false,
+    activeMarker: null
+  })
+}
+}
+
+render() {
+return (
+  <div classTitle = 'map-container' style= 
+{{marginleft:'250px'}}>
+    <div>
+      <div classTitle = 'sideMenu'>
+        <div classTitle = 'PlaceList'>
+          <ol classTitle='Places'>
+            {events.map((event, index)=> 
+              <li key = {index} classTitle='Place' onClick={() => {this.onLiClick(index)}}>
+                {event.title}
+              </li>
+            )}
+          </ol>
+        </div>
+      </div>
+    </div>
+    <Map google={this.props.google} zoom={14}
+      initialCenter = {{lat:40.7589, lng:-73.9851}} onClick={this.onMapClicked}>
+      {events.map((marker, i) => 
+          <Marker
+          ref={(e) => {if (e) this.markers[i] = 
+ e.marker}}
+          onClick={this.onMarkerClick}
+          title = {marker.title}
+          name={marker.title}
+          position = 
+{{lat:marker.lat,lng:marker.lng}}
+          />
+      )}
+      <InfoWindow
+        onOpen={this.windowHasOpened}
+        onClose={this.windowHasClosed}
+        marker={this.state.activeMarker}
+        visible={this.state.showingInfoWindow}>
+        <div>
+          <h1>{this.state.selectedPlace.title}</h1>
+          {/* <h3>{this.state.selectedPlace.Description}</h3>
+          <h3>{this.state.selectedPlace.category}</h3>
+          <h3>{this.state.selectedPlace.totalGuests}</h3>
+          <h3>{this.state.selectedPlace.time}</h3> */}
+        </div>
+      </InfoWindow>
+    </Map>
+  </div>
+ );
+ }
+}
+
+export default GoogleApiWrapper({
+apiKey: 'AIzaSyDd5tCW_eNmH7IGrTQvgsSs4v5QS1VTank'
+})(MapContainer)
