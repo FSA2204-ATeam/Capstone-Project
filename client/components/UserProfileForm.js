1daa3cb359c1cab66aab6063e1985a0c22549f4b
Erion-X <98784085+Erion-X@users.noreply.github.com>
2022-07-01 19:13:59 -0400

    User update form render only but validators not working

---
+++ client/components/UserProfileForm.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import FormInput from './FormInput';
+
+export const UserProfileForm = () => {
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    firstname: '',
+    lastname: '',
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: 'username',
+      type: 'text',
+      placeholder: 'username',
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
+      required: true,
+    },
+    {
+      id: 2,
+      name: 'email',
+      type: 'email',
+      placeholder: 'email',
+      errorMessage: 'It should be a valid email address!',
+      label: 'Email',
+      required: true,
+    },
+    {
+      id: 3,
+      name: 'firstname',
+      type: 'text',
+      placeholder: 'First Name',
+      errorMessage: 'First name should be 1-16 characters!',
+      label: 'First Name',
+      pattern: '^[A-Za-z0-9]{1,16}$',
+      required: true,
+    },
+    {
+      id: 4,
+      name: 'lastname',
+      type: 'text',
+      placeholder: 'Last Name',
+      errorMessage: 'Last name should be 1-16 characters!',
+      label: 'First Name',
+      pattern: '^[A-Za-z0-9]{1,16}$',
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  return (
+    <div className="app">
+      testestest
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserProfileForm;
