29ac198a19b66cfe733afda9b7c8800388154939
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-07-07 14:25:29 -0500

    Cleaned up function. Ready for deployment

--- client/components/common/Randomizer.js
+++ client/components/common/Randomizer.js
@@ -1,19 +1,7 @@
 const Randomizer = (dataset) => {
   if (!dataset) return {};
 
-  // let numOfEvents = dataset.length;
-  // let random = Math.floor(Math.random() * numOfEvents);
-
-  // return dataset[random];
-
-  // alternative form
-
   return dataset[Math.floor(Math.random() * dataset.length)];
 };
 
 module.exports = Randomizer;
-
-// . events length was 41
-// math .random will result in a number such  as 0.4516
-// 0.4516 * 41 = 18.51
-// math floor rounds down to 18
