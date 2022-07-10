3cea3c8c87ebea03b7917c8047ae5f555ebe8d38
Erion-X <98784085+Erion-X@users.noreply.github.com>
2022-07-10 17:06:50 -0400

    Modified findOrCreate axios.post on usersevents file to find by entire req.body rather than databaseId. This way a false databaseId is not required to create user generated events, it just checks for an exact match given all the input fields passed from the client.

--- client/components/NewEventForm.js
+++ client/components/NewEventForm.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 export const NewEventForm = (latLng) => {
   const [values, setValues] = useState({
-    databaseId: 'testest',
     name: '',
     shortDesc: '',
     datePart: '',
--- server/api/usersevents.js
+++ server/api/usersevents.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
-const axios = require("axios");
+const router = require('express').Router();
+const axios = require('axios');
 const {
   models: { Event, User, UsersEvents },
-} = require("../db");
+} = require('../db');
 
-const { requireToken, isAdmin } = require("../api/gateKeepingMiddleware");
+const { requireToken, isAdmin } = require('../api/gateKeepingMiddleware');
 module.exports = router;
 
-router.post("/", requireToken, async (req, res, next) => {
+router.post('/', requireToken, async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
     const [newEvent, created] = await Event.findOrCreate({
-      where: { databaseId: req.body.databaseId },
+      where: req.body,
       defaults: req.body,
     });
     await newEvent.setUsers(user);
@@ -21,7 +21,7 @@ router.post("/", requireToken, async (req, res, next) => {
   }
 });
 
-router.get("/", requireToken, async (req, res, next) => {
+router.get('/', requireToken, async (req, res, next) => {
   try {
     const user = await User.findByToken(req.headers.authorization);
     const data = await UsersEvents.findAll({
@@ -31,7 +31,7 @@ router.get("/", requireToken, async (req, res, next) => {
     });
     //   include: { model: Event, where: { eventId: { $col: "id" } } },
     // });
-    console.log("Associations -->", data);
+    console.log('Associations -->', data);
     res.json(data);
   } catch (error) {
     next(error);
