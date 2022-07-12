6dbcaa6cb9d724c9e3abb03495b2886b2efba3ba
walkerfisher <101424637+walkerfisher@users.noreply.github.com>
2022-07-12 09:26:21 -0700

    Duplicate clean up for pushing cleared request for pulling EventButtons

--- client/store/singleEvent.js
+++ client/store/singleEvent.js
@@ -1,17 +1,11 @@
 import axios from "axios";
 
 const SET_USER_EVENT = "SET_USER_EVENT";
-//const SET_USER_RSVP = "SET_USER_RSVP";
 
 export const _setUserEvent = (event) => ({
   type: SET_USER_EVENT,
   event,
 });
-
-// export const _setUserRSVP = (idx) => ({
-//   type: SET_USER_RSVP,
-//   idx,
-// });
 
 export const setUserEvent = (event) => async (dispatch) => {
   try {
@@ -22,22 +16,10 @@ export const setUserEvent = (event) => async (dispatch) => {
   }
 };
 
-// export const setUserRSVP = (id) => async (dispatch) => {
-//   try {
-//     const { data: userRSVP } = await axios.put("/api/userRSVP", id);
-//     dispatch(_setUserRSVP(userRSVP));
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-//is state an object {} here?
 const singleEventReducer = (state = {}, action) => {
   switch (action.type) {
     case SET_USER_EVENT:
       return action.event;
-    // case SET_USER_RSVP:
-    //   return [...state, action.userRSVP];
     default:
       return state;
   }
