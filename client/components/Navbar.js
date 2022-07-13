18f4c0e2f7dbb79fc73c0393230a14f19330dbac
Erion-X <98784085+Erion-X@users.noreply.github.com>
2022-07-12 20:12:25 -0400

    bug fixes

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,13 +1,13 @@
-import { logout } from "../store";
-import { connect } from "react-redux";
-import React from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
-import SearchIcon from "@material-ui/icons/Search";
-import { useNavStyles } from "../theme";
-import { Link } from "react-router-dom";
+import { logout } from '../store';
+import { connect } from 'react-redux';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import { useNavStyles } from '../theme';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const classes = useNavStyles();
@@ -18,7 +18,8 @@ const Navbar = () => {
           <Typography className={classes.title} variant="h4" noWrap>
             URBAN SAFARI
           </Typography>
-          <Link to="./events/myevents">MY EVENTS</Link>
+          <Link to="/events/myevents">MY EVENTS</Link>
+          <Link to="/profile">PROFILE</Link>
         </Toolbar>
       </AppBar>
     </div>
