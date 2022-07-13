82a03c3dfac53e3b24efbf6c7076d98ad5d94aa3
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-07-12 21:22:58 -0500

    api request to database is working correctly.

--- server/api/events.js
+++ server/api/events.js
@@ -6,52 +6,69 @@ const {
 const User = require("../db/models/User");
 const formatDate = require("../../script/formatDate");
 const { requireToken, isAdmin } = require("../api/gateKeepingMiddleware");
+const { Op } = require("sequelize");
 
 // Gets event list from NYC API
 router.get("/", async (req, res, next) => {
-  let startDate = formatDate(0);
-  let endDate = formatDate(1);
+  let [searchStart, searchEnd, startDate, endDate] = formatDate(1);
+
+  console.log("searchStart: ", searchStart);
+  console.log("searchEnd: ", searchEnd);
+  console.log("startDate: ", startDate);
+  console.log("endDate: ", endDate);
 
   let addressUrl = `https://api.nyc.gov/calendar/search?startDate=${startDate} 12:00 AM&endDate=${endDate} 12:00 AM&pageNumber=`;
   let events = [];
 
   try {
-  } catch (error) {}
-
-  for (let pgno = 1; pgno <= 5; pgno++) {
-    try {
-      const { data } = await axios.get(`${addressUrl}${pgno}`, {
-        headers: {
-          "Cache-Control": "no-cache",
-          "Ocp-Apim-Subscription-Key": `${process.env.NYC_EVENTS_API_KEY}`,
+    const result = await Event.findAll({
+      where: {
+        startDate: {
+          [Op.and]: [{ [Op.gte]: searchStart }, { [Op.lte]: searchEnd }],
         },
-      });
-
-      data.items
-        .filter((e) => e.geometry !== undefined)
-        .map((evt) =>
-          events.push({
-            name: evt.name,
-            shortDesc: evt.shortDesc,
-            startDate: evt.startDate,
-            endDate: evt.endDate,
-            permalink: evt.permalink,
-            address: evt.address,
-            eventLat: evt.geometry[0].lat,
-            eventLng: evt.geometry[0].lng,
-            databaseId: evt.id.toString(),
-            source: "NYC API ",
-          })
-        );
-
-      const { creationResult } = await Event.bulkCreate(events, {
-        ignoreDuplicates: true,
-      });
-    } catch (error) {
-      next(error);
-    }
+      },
+    });
+  } catch (err) {
+    next(err);
   }
-  res.send(events);
+
+  // try {
+  // } catch (error) {}
+
+  // for (let pgno = 1; pgno <= 5; pgno++) {
+  //   try {
+  //     const { data } = await axios.get(`${addressUrl}${pgno}`, {
+  //       headers: {
+  //         "Cache-Control": "no-cache",
+  //         "Ocp-Apim-Subscription-Key": `${process.env.NYC_EVENTS_API_KEY}`,
+  //       },
+  //     });
+
+  //     data.items
+  //       .filter((e) => e.geometry !== undefined)
+  //       .map((evt) =>
+  //         events.push({
+  //           name: evt.name,
+  //           shortDesc: evt.shortDesc,
+  //           startDate: evt.startDate,
+  //           endDate: evt.endDate,
+  //           permalink: evt.permalink,
+  //           address: evt.address,
+  //           eventLat: evt.geometry[0].lat,
+  //           eventLng: evt.geometry[0].lng,
+  //           databaseId: evt.id.toString(),
+  //           source: "NYC API ",
+  //         })
+  //       );
+
+  //     const { creationResult } = await Event.bulkCreate(events, {
+  //       ignoreDuplicates: true,
+  //     });
+  //   } catch (error) {
+  //     next(error);
+  //   }
+  // }
+  // res.send(events);
 });
 
 // feeling wild get route
