7992ef26ba940c86146d5928a0d167d30f1999ed
Tim Brandau <96590473+tacticalbison@users.noreply.github.com>
2022-07-15 16:32:25 -0500

    mapSingleEvent state not quite right - review the MyEvent.js for guidance

--- client/components/MapSingleEvent.js
+++ client/components/MapSingleEvent.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setUserRSVP, fetchUserEvents } from "../store/usersEvents";
+import {
+  setUserEvents,
+  setUserRSVP,
+  fetchUserEvents,
+} from "../store/usersEvents";
+
 import { InfoWindow } from "@react-google-maps/api";
-import { friendlyDate } from "../../script/formatDate";
 
 const MapSingleEvent = ({ randomOrder }) => {
   const dispatch = useDispatch();
 
+  const user = useSelector((state) => state.auth);
+  const myEvents = useSelector((state) => state.usersEvents.events);
   const allEvents = useSelector((state) => state.events.events);
-  const usersEvents = useSelector((state) => state.usersEvents.events);
 
-  const [myAssociations, setMyAssociations] = useState(usersEvents);
-  console.log("ASS", myAssociations);
+  const [myAssociations, setMyAssociations] = useState(myEvents);
+
   const [idx, setIdx] = useState(0);
-  const [myEvents, setMyEvents] = useState(usersEvents);
   const [activeEvent, setActiveEvent] = useState(allEvents[randomOrder[idx]]);
 
   useEffect(() => {
-    setMyEvents(usersEvents);
-  }, [usersEvents]);
+    dispatch(setUserEvents(user.id));
+    dispatch(fetchUserEvents());
+  }, []);
 
   useEffect(() => {
+    console.log("did idx update?", idx);
     setActiveEvent(allEvents[randomOrder[idx]]);
   }, [idx]);
 
   useEffect(() => {
-    setMyAssociations(usersEvents.map((ele) => ele.id));
-  }, [usersEvents]);
+    console.log("did usersEvents update my associations?", myAssociations);
+    setMyAssociations(myEvents.map((ele) => ele.id));
+  }, [idx]);
 
   const onRSVPClick = (activeEvent) => {
     dispatch(setUserRSVP(activeEvent));
