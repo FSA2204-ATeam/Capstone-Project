070c7b8bda23afa97036fc8050a6eac57a44cd1b
Erion-X <98784085+Erion-X@users.noreply.github.com>
2022-07-19 18:25:56 -0400

    Fixed profile update and added user feedback on successful db update

--- client/components/UserProfileForm.js
+++ client/components/UserProfileForm.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import FormInput from "./FormInput";
-import { updateProfile } from "../store/auth";
-import { updatePreferences } from "../store/userPreferences";
-import axios from "axios";
-import { Button } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import FormInput from './FormInput';
+import { updateProfile } from '../store/auth';
+import { updatePreferences } from '../store/userPreferences';
+import axios from 'axios';
+import { Button } from '@material-ui/core';
 
 const UserProfileForm = (props) => {
   const [values, setValues] = useState({
-    username: props.userProfile.username || "",
-    email: props.userProfile.email || "",
-    firstname: props.userProfile.firstname || "",
-    lastname: props.userProfile.lastname || "",
+    username: props.userProfile.username || '',
+    email: props.userProfile.email || '',
+    firstname: props.userProfile.firstname || '',
+    lastname: props.userProfile.lastname || '',
   });
 
   const [categoryPreferences, setCategoryPreferences] = useState(
@@ -20,17 +20,17 @@ const UserProfileForm = (props) => {
 
   useEffect(() => {
     setValues({
-      username: props.userProfile.username || "",
-      email: props.userProfile.email || "",
-      firstname: props.userProfile.firstname || "",
-      lastname: props.userProfile.lastname || "",
+      username: props.userProfile.username || '',
+      email: props.userProfile.email || '',
+      firstname: props.userProfile.firstname || '',
+      lastname: props.userProfile.lastname || '',
     });
   }, [props.userProfile]);
 
   useEffect(() => {
     setCategoryPreferences(
       Object.keys(props.userPreferences)
-        .filter((key) => key.includes("CAT_"))
+        .filter((key) => key.includes('CAT_'))
         .reduce((obj, key) => {
           return Object.assign(obj, {
             [key]: props.userPreferences[key],
@@ -42,57 +42,57 @@ const UserProfileForm = (props) => {
   const inputs = [
     {
       id: 1,
-      name: "username",
-      type: "text",
-      placeholder: "username",
+      name: 'username',
+      type: 'text',
+      placeholder: 'username',
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Username",
-      pattern: "^[A-Za-z0-9]{3,16}$",
+      label: 'Username',
+      pattern: '^[A-Za-z0-9]{3,16}$',
       required: true,
     },
     {
       id: 2,
-      name: "email",
-      type: "email",
-      placeholder: "email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
+      name: 'email',
+      type: 'email',
+      placeholder: 'email',
+      errorMessage: 'It should be a valid email address!',
+      label: 'Email',
       required: true,
     },
     {
       id: 3,
-      name: "firstname",
-      type: "text",
-      placeholder: "First Name",
-      errorMessage: "First name should be 1-16 characters!",
-      label: "First Name",
-      pattern: "^[A-Za-z0-9]{1,16}$",
+      name: 'firstname',
+      type: 'text',
+      placeholder: 'First Name',
+      errorMessage: 'First name should be 1-16 characters!',
+      label: 'First Name',
+      pattern: '^[A-Za-z0-9]{1,16}$',
       required: true,
     },
     {
       id: 4,
-      name: "lastname",
-      type: "text",
-      placeholder: "Last Name",
-      errorMessage: "Last name should be 1-16 characters!",
-      label: "Last Name",
-      pattern: "^[A-Za-z0-9]{1,16}$",
+      name: 'lastname',
+      type: 'text',
+      placeholder: 'Last Name',
+      errorMessage: 'Last name should be 1-16 characters!',
+      label: 'Last Name',
+      pattern: '^[A-Za-z0-9]{1,16}$',
       required: true,
     },
   ];
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const token = window.localStorage.getItem("token");
+    const token = window.localStorage.getItem('token');
     if (token) {
-      const userRes = await axios.put("api/users/updateProfile", values, {
+      const userRes = await axios.put('api/users/updateProfile', values, {
         headers: {
           authorization: token,
         },
       });
       const prefsRes = await axios.put(
-        "api/users/preferences",
+        'api/users/preferences',
         categoryPreferences,
         {
           headers: {
@@ -100,6 +100,10 @@ const UserProfileForm = (props) => {
           },
         }
       );
+      console.log(userRes, prefsRes);
+      if (userRes.status === 200 && prefsRes.status === 200) {
+        updtSubmissionFeedback();
+      }
       props.updateState(userRes.data, prefsRes.data);
     }
   };
@@ -115,10 +119,19 @@ const UserProfileForm = (props) => {
     });
   };
 
+  //UPDATE SUBMIT FEEDBACK
+  const [successfulUpdt, setSuccessfulUpdt] = useState(false);
+  const updtSubmissionFeedback = () => {
+    setSuccessfulUpdt(true);
+  };
+
   return (
     <div align="center">
       <form onSubmit={handleSubmit}>
-        <h1 align="center" style={{fontFamily: "Shrikhand"}}>{`${props.userProfile.username
+        <h1
+          align="center"
+          style={{ fontFamily: 'Shrikhand' }}
+        >{`${props.userProfile.username
           .slice(0, 1)
           .toUpperCase()}${props.userProfile.username.slice(1)}'s Profile`}</h1>
         {inputs.map((input) => (
@@ -137,8 +150,8 @@ const UserProfileForm = (props) => {
                   type="button"
                   className={
                     categoryPreferences[category]
-                      ? "categoryButton Selected"
-                      : "categoryButton"
+                      ? 'categoryButton Selected'
+                      : 'categoryButton'
                   }
                   onClick={() => handleCatSelect(category)}
                 >
@@ -147,9 +160,31 @@ const UserProfileForm = (props) => {
               ))
             : null}
         </div>
-        <div>
-          <Button>Update</Button>
-        </div>
+        <p>
+          {successfulUpdt ? (
+            <Button
+              type={'submit'}
+              style={{
+                backgroundColor: '#F9DB53',
+                padding: '5px 0px',
+                fontSize: '10px',
+              }}
+            >
+              Submitted!
+            </Button>
+          ) : (
+            <Button
+              type={'submit'}
+              style={{
+                backgroundColor: '#F5F5F5',
+                padding: '5px 0px',
+                fontSize: '10px',
+              }}
+            >
+              Submit
+            </Button>
+          )}
+        </p>
       </form>
     </div>
   );
